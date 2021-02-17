8b8e63d56aed27357d1587ec213f7377b68a1d29
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2021-02-17 17:40:28 +0400

    modify the project structure

--- ShopApp/Helpers/Constants/AppImages.swift
+++ ShopApp/Helpers/Constants/AppImages.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+enum Images: String {
+  case image
+}
--- ShopApp/Helpers/Constants/Constants.swift
+++ ShopApp/Helpers/Constants/Constants.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum Constants {
+  
+  enum urls {
+    static let host = "ey3f2y0nre.execute-api.us-east-1.amazonaws.com"
+    static let scheme = "https"
+    static let path = "/default/"
+  }
+  
+  enum errors {
+    static var somethingWentWrong = NSError(domain: "shopapp",
+                                            code: -990,
+                                            userInfo: [NSLocalizedDescriptionKey:
+                                              "generic_something_went_wrong_msg"])
+    static var jsonSerializingFailure = NSError(domain: "shopapp", code: -991, userInfo: [NSLocalizedDescriptionKey:
+                                                                                    "json_serializing_failure"])
+  }
+  
+  enum strings {
+    static let key = "$$amatoa"
+  }
+  
+  enum ints { }
+  
+}
