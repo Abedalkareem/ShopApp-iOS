cf7d2c5ecea343544bdcb7b36427013fe5b48a37
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2021-02-17 18:45:33 +0400

    add base view controllers

---
+++ ShopApp/ViewControllers/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  ShopApp
+//
+//  Created by abedalkareem omreyh on 17/02/2021.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
---
+++ ShopApp/ViewControllers/Base/BaseViewModelController.swift
@@ -0,0 +1,61 @@
+//
+//  BaseViewModelController.swift
+//  ShopApp
+//
+//  Created by abedalkareem omreyh on 17/02/2021.
+//
+
+import RxSwift
+import UIKit
+
+///
+/// Any `ViewController` need to have a view model it should inherit this
+/// class. this class will provid an injected `viewModel`.
+///
+/// An example:
+/// ```
+/// class SplashViewController: BaseViewModelController<SplashViewModel> {
+///   .....
+///   private func observeForItems() {
+///     viewModel.items
+///       .subscribe(onNext: {
+///   .....
+/// }
+/// ```
+///
+class BaseViewModelController<ViewModel: ViewModelType>: BaseViewController {
+  
+  // MARK: - Properties
+  @Injected var viewModel: ViewModel
+  
+  var disposeBag = DisposeBag()
+  
+  // MARK: Private properties
+  // MARK: - ViewController lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  private func observeForErrors() {
+    guard let errorEmitable = viewModel as? ErrorEmitable else {
+      return
+    }
+    errorEmitable.error
+      .subscribe(onNext: { error in
+        // TODO: Show error
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  private func observeForLoading() {
+    guard let loadable = viewModel as? Loadable else {
+      return
+    }
+    loadable.loading
+      .subscribe(onNext: { isLoading in
+        // TODO: Show loading
+      })
+      .disposed(by: disposeBag)
+  }
+}
